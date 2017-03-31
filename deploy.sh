bee8053cdfa36e8999e26853ecbb04bc40293d52
Georgi Zlatkov <georgi@passfort.com>
2017-03-31 15:15:34 +0100

    Seems like env variables are not propageted properly

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ export PROJECT_ID=core-gearbox-112418
 export TREE_HASH=`git cat-file -p HEAD`
 export TREE_HASH=${TREE_HASH:5:7}
 export CONTAINER_TAG=$TREE_HASH
+export CLUSTER_NAME=production
 
 printer()
 {
@@ -25,9 +26,9 @@ deploy_container()
 }
 
 # Switch to the correct cluster
-sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster $CLUSTER_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
-sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials $CLUSTER_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster production
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone europe-west1-b
+sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials production
 
 printer "Building Release *$CONTAINER_TAG*"
 build_container
