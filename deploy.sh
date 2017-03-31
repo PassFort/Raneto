e20616984298d59d302efdb7539d061f71872944
Georgi Zlatkov <georgi@passfort.com>
2017-03-31 15:07:59 +0100

    We aslo need to set the compute zone

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ export PROJECT_ID=core-gearbox-112418
 export TREE_HASH=`git cat-file -p HEAD`
 export TREE_HASH=${TREE_HASH:5:7}
 export CONTAINER_TAG=$TREE_HASH
-export CLUSTER=${1:-staging}
 
 printer()
 {
@@ -26,13 +25,14 @@ deploy_container()
 }
 
 # Switch to the correct cluster
-sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster production
-sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials production
+sudo /opt/google-cloud-sdk/bin/gcloud config set container/cluster $CLUSTER_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials $CLUSTER_NAME
 
 printer "Building Release *$CONTAINER_TAG*"
 build_container
 printer "Build completed"
 
-printer "Deploying Release *$CONTAINER_TAG* to cluster *$CLUSTER* ($CLUSTER_NAME)."
+printer "Deploying Release *$CONTAINER_TAG* to cluster *$CLUSTER_NAME* ($CLUSTER_NAME)."
 deploy_container
 printer "Deploy completed"
